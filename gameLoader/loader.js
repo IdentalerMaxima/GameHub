dd01a6572baa051c065daa79f8fc4ccdf54496fb
IdentalerMaxima <koppany.naray@gmail.com>
2023-05-29 13:18:44 +0200

    added loader and structure

---
+++ gameLoader/loader.js
@@ -0,0 +1,29 @@
+//Loader for games, first delete the old script and then load the new one
+function loadGame(gameFile) {
+
+    var gameContainer = document.getElementById('gameContainer');
+    gameContainer.innerHTML = '';
+
+
+    var script = document.createElement('script');
+    script.src = gameFile;
+
+    gameContainer.appendChild(script);
+}
+
+//Add event listeners to the buttons
+document.getElementById('game1').addEventListener('click', function () {
+    loadGame('game1.js');
+});
+
+document.getElementById('chess').addEventListener('click', function () {
+    loadGame('games/Chess/Chess.js');
+});
+
+document.getElementById('game3').addEventListener('click', function () {
+    loadGame('game3.js');
+});
+
+document.getElementById('game4').addEventListener('click', function () {
+    loadGame('game4.js');
+});
---
+++ games/Chess/Chess.js
@@ -0,0 +1,1 @@
+console.log("Chess.js loaded");
